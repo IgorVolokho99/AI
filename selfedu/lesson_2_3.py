d9f784615cf8ea4d338f5e6f7d6e6b10c51c2936
Igor Volokho <igorvolokho99@gmail.com>
2025-02-05 23:19:22 +0300

    #4 Second lesson with simple example(Task XOR).

--- selfedu/lesson_2_3.py
+++ selfedu/lesson_2_3.py
@@ -1,33 +1,26 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 
-def act(x):
-    return 1 if x >= 0 else 0
+def act(x: float) -> int:
+    return 1 if x <= 0 else 0
 
 
-def go(C):
-    x = np.array([C[0], C[1], 1])
-    w1 = np.array([1, 1, -1.5])
-    w2 = np.array([1, 1, -0.5])
-    w_hidden = np.array([w1, w2])
-    sum_hidden = np.dot(w_hidden, x)
+def check_xor(x: float, y: float) -> int:
+    x_input = np.array([x, y, 1])
+    w_1 = [1, 1, -1.5]
+    w_2 = [1, 1, -0.5]
+    w_hidden = np.array([w_1, w_2])
+    w_out = np.array([-1, 1, 0.5])
 
-    act_hidden = np.array([act(i) for i in sum_hidden] + [-1])
+    x_sum_1 = np.dot(w_hidden, x_input)
+    x_out_1 = [act(x) for x in x_sum_1]
+    x_out_1.append(1)
+    x_out_1 = np.array(x_out_1)
 
-    w_hidden_2 = np.array([-1, 1, 0.5])
-    res = np.dot(w_hidden_2, act_hidden)
+    x_sum_2 = np.dot(w_out, x_out_1)
+    res = act(x_sum_2)
 
-    if act(res) == 1:
-        print("Class 1")
-    else:
-        print("Class 2")
+    return res
 
 
-C1 = [(0, 1), (1, 0)]
-C2 = [(0, 0), (1, 1)]
-go(C1[0])
-go(C1[1])
-
-go(C2[0])
-go(C2[1])
+print(check_xor(1, 0))
