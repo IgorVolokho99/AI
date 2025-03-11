2ed93e99c41fba82c7a60639202a15325831c572
Igor Volokho <igorvolokho99@gmail.com>
2025-03-11 22:42:49 +0300

    #4 Redid second code.

--- selfedu/lesson_2.py
+++ selfedu/lesson_2.py
@@ -1,23 +1,29 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 N = 5
-x1 = np.random.random(N)
-x2 = x1 + [np.random.randint(10) / 10 for _ in range(N)]
-C1 = [x1, x2]
 
 x1 = np.random.random(N)
-x2 = x1 - [np.random.randint(10) / 10 for _ in range(N)] - 0.1
+x2 = x1 + [np.random.randint(10) / 10 for i in range(N)]
+C1 = [x1, x2]
+
+x1 = np.random.random(5)
+x2 = x1 - [np.random.randint(10) / 10 for i in range(N)] - 0.1
 C2 = [x1, x2]
 
 f = [0, 1]
 
-w = np.array([-0.3, 0.3])
-
+weights = np.array([-0.3, 0.3])
 for i in range(N):
-    x_input = np.array([C2[0][i], C2[1][i]])
-    s = np.dot(w, x_input)
-
-    if s >= 0:
+    x = np.array([C2[0][i], C2[1][i]])
+    y = np.dot(x, weights)
+    if y >= 0:
         print("Класс 1")
     else:
         print("Класс 2")
+
+# plt.plot(C1[0][:], C1[1][:], s=10, c="red")
+# plt.plot(C2[0][:], C2[1][:], s=10, c="blue")
+# plt.plot(f)
+# plt.grid(True)
+# plt.show()
