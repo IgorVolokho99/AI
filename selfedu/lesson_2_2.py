124d08ffae15e7996ddccfad8af00dbcd0d565f6
Igor Volokho <igorvolokho99@gmail.com>
2025-03-11 23:06:43 +0300

    #4 Redid second code.

--- selfedu/lesson_2_2.py
+++ selfedu/lesson_2_2.py
@@ -3,25 +3,22 @@ import numpy as np
 N = 5
 b = 3
 
-x1 = np.random.random(N)
-x2 = x1 + [np.random.randint(10) / 10 for _ in range(N)] + b
+x1 = np.random.random(5)
+x2 = x1 + [np.random.randint(10) / 10 for _ in range(5)] + b
 C1 = [x1, x2]
 
-x1 = np.random.random(N)
-x2 = x1 - [np.random.randint(10) / 10 for _ in range(N)] + b - 0.1
+x1 = np.random.random(5)
+x2 = x1 - [np.random.randint(10) / 10 for _ in range(5)] - b - 0.1
 C2 = [x1, x2]
 
-f = [0 + b, 1 + b]
-
-w2 = 0.3
+w1 = -0.3
+w2 = -w1
 w3 = -b * w2
-weights = np.array([-w2, w2, w3])
-
+weights = np.array([w1, w2, w3])
 for i in range(N):
-    x_input = np.array([C2[0][i], C2[1][i], 1])
-    s = np.dot(weights, x_input)
-
-    if s >= 0:
-        print("C1")
+    x = np.array([C2[0][i], C2[1][i], 1])
+    y = np.dot(x, weights)
+    if y >= 0:
+        print("Класс 1")
     else:
-        print("C2")
+        print("Класс 2")
