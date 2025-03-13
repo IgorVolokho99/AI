0dc3690365293d96e637fd41eae725c671632d4c
Igor Volokho <igorvolokho99@gmail.com>
2025-03-13 23:24:15 +0300

    #6 Redid third code.

--- selfedu/lesson_3_1.py
+++ selfedu/lesson_3_1.py
@@ -6,28 +6,29 @@ def f(x):
 
 
 def df(x):
-    return 0.5 * (1 - x) * (1 + x)
+    return 0.5 * (1 + x) * (1 - x)
 
 
-W1 = np.array([[-0.2, 0.3, -0.4], [0.1, -0.3, -0.4]])
-W2 = np.array([0.2, 0.3])
+W1 = np.array([[-0.267867, 0.6, -0.4], [0.567, -0.3876, -0.84]])
+W2 = np.array([0.6, -0.3])
 
 
 def go_forward(inp):
-    sum = np.dot(W1, inp)
-    out = np.array([f(x) for x in sum])
+    sum1 = np.dot(W1, inp)
+    out1 = np.array([f(x) for x in sum1])
 
-    sum = np.dot(W2, out)
-    y = f(sum)
-    return (y, out)
+    sum2 = np.dot(W2, out1)
+    y = f(sum2)
+
+    return y, out1
 
 
 def train(epoch):
-    global W2, W1
-    lmd = 0.01
-    N = 10000
+    global W1, W2
+    lmd = 0.05
+    N = 1_000_000
     count = len(epoch)
-    for k in range(N):
+    for i in range(N):
         x = epoch[np.random.randint(0, count)]
         y, out = go_forward(x[0:3])
         e = y - x[-1]
@@ -36,21 +37,24 @@ def train(epoch):
         W2[1] = W2[1] - lmd * delta * out[1]
 
         delta2 = W2 * delta * df(out)
-        W1[0, :] = W1[0, :] - np.array(x[0:3]) * delta2[0] * lmd
-        W1[1, :] = W1[1, :] - np.array(x[0:3]) * delta2[1] * lmd
+
+        W1[0, :] = W1[0, :] - lmd * delta2[0] * np.array(x[0:3])
+        W1[1, :] = W1[1, :] - lmd * delta2[1] * np.array(x[0:3])
 
 
-epoch = [(-1, -1, -1, -1),
-         (-1, -1, 1, 1),
-         (-1, 1, -1, -1),
-         (-1, 1, 1, 1),
-         (1, -1, -1, -1),
-         (1, -1, 1, 1),
-         (1, 1, -1, -1),
-         (1, 1, 1, -1)]
+epoch = [
+    (-1, -1, -1, -1),
+    (-1, -1, 1, 1),
+    (-1, 1, -1, -1),
+    (-1, 1, 1, 1),
+    (1, -1, -1, -1),
+    (1, -1, 1, 1),
+    (1, 1, -1, -1),
+    (1, 1, 1, -1)
+]
 
 train(epoch)
 
 for x in epoch:
     y, out = go_forward(x[0:3])
-    print(f"Выходные значения НC: {y} -> {x[-1]}")
+    print(f"Полученный результат НС: {y} -> {x[-1]}")
